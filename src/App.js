b380640bbff3e912dae6c8a28101c5aa787dec40
Dgordgun <georgdvorskii@gmail.com>
2024-09-21 15:26:07 +0300

    Add first react to github

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewCompon from './NewCompon.js';
+const App = (props)=>{
+    const obj = {
+        fN:'Vova',
+        lN:'Dvorskii'
+    }
+    return <NewCompon/>
+}
+export default App
---
+++ src/NewCompon.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function NewCompon(promps) {
+    return 'Hi'
+}
+
+export default NewCompon
