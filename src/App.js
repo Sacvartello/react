2cadd56dd5c7d61b94fbbc3ae296e264d9ffb32d
Dgordgun <georgdvorskii@gmail.com>
2024-09-21 16:06:57 +0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Aloha from './components/Aloha';
 import'./style.css';
 const App = (props)=>{
     const obj = {
         fN:'Vova',
         lN:'Dvorskii'
     }
-    return <h1 className='red-text'>Hi {obj.fN} {obj.lN}</h1>
+    return <h1 className='red-text'><Aloha/> {obj.fN} {obj.lN}</h1>
 }
 export default App
---
+++ src/components/Aloha/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { jsxs } from "react/jsx-runtime";
+
+
+function Aloha(){
+    return 'Hi'
+}
+
+export default Aloha
