dc887033a79026a0005da938fa9da0455bdee971
Dgordgun <georgdvorskii@gmail.com>
2024-09-22 21:06:38 +0300

    timer with 1btn

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -4,16 +4,17 @@ class Timer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            time: new Date(0,0,0,0,0,0,0)
+            time: new Date(0,0,0,0,0,0,0),
+            isRunning :false
         }
         this.intervalId= null
-        
     }
     start=()=>{
         if(this.intervalId === null){
             this.intervalId = setInterval(()=>{
                 this.setState({
-                    time:addSeconds(this.state.time, 1)
+                    time:addSeconds(this.state.time, 1),
+                    isRunning:true
                 })
             }, 1000)
         }
@@ -21,6 +22,9 @@ class Timer extends Component {
     stop=()=>{
         clearInterval(this.intervalId)
         this.intervalId =null
+        this.setState({
+            isRunning:false
+        })
     }
     restart=()=>{
         this.setState({
@@ -31,11 +35,14 @@ class Timer extends Component {
         this.stop()
     }
     render() {
+        const callback = this.state.isRunning? this.stop: this.start
+        const textOnBtn = this.state.isRunning? 'Stop':'Start'
         return (
             <div>
                 <h1>{format(this.state.time,'HH:mm:ss')}</h1>
-                <button onClick={this.start}>Start</button>
-                <button onClick={this.stop}>Stop</button>
+                {/* <button onClick={this.start}>Start</button>
+                <button onClick={this.stop}>Stop</button> */}
+                <button onClick={callback}>{textOnBtn}</button>
                 <button onClick={this.restart}>Restart</button>
             </div>
         );
