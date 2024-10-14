f46c9f3da8031dd30b7f42735ce72a11e954558c
Dgordgun <georgdvorskii@gmail.com>
2024-10-14 20:38:45 +0300

    add scss, sass,  module css

--- src/components/PageUsers/User/index.js
+++ src/components/PageUsers/User/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import './style.css'
+import styles from'./style.module.css'
 const User=(props)=>{
     const {name:{first,last}, picture:{medium},location:{city,country
     },email,phone} = props.user
     return(
-        <article className='card'>
-            <img className='img' src={medium}/>
-            <h2 className='us-name'>{first} {last}</h2>
-            <p className='loc'>{city}, {country}</p>
-            <p className='email'>email: {email}</p>
-            <p className='phone'>phone: {phone}</p>
+        <article className={styles.card}>
+            <img className={styles.img} src={medium}/>
+            <h2>{first} {last}</h2>
+            <p>{city}, {country}</p>
+            <p className={styles.email}>email: {email}</p>
+            <p className={styles.phone}>phone: {phone}</p>
         </article>
     )
 }
