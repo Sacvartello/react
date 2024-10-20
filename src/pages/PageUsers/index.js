6f6d7d653e4e88de5b9fa46b0b2b48e07271c277
Dgordgun <georgdvorskii@gmail.com>
2024-10-20 18:31:18 +0300

    Spinner + error handling

--- src/pages/PageUsers/index.js
+++ src/pages/PageUsers/index.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 import User from './../../components/User';
 import './style.css'
+import Spinner from '../../components/Spinner';
 class PageUsers extends Component {
     constructor(props) {
         super(props)
         this.state ={
-            users:[]
+            users:[],
+            isLoading:false,
+            error: null
         }
     }
     componentDidMount(){
+        this.setState({
+            isLoading:true
+        })
         fetch('https://randomuser.me/api/?results=50')
         .then(ret =>ret.json())
         .then(data =>{
             console.log(data);
             this.setState({
-                users:data.results
+                users:data.results,
             })
         })
+        .catch(err=>{
+            this.setState({
+                error:err
+            })
+        })
+        .finally(()=>{
+            this.setState({
+                isLoading:false
+            })
+        })
+        
     }
     render() {
         const layout = this.state.users.map(us => <User user={us} key={us.login.uuid}/>)
+        const errMes = <p>Opps, something went wrong(</p>
         return (
             <section className='page'>
+                {this.state.error && errMes}
+                {this.state.isLoading && <Spinner/>}
                 {layout}
             </section>
         );
