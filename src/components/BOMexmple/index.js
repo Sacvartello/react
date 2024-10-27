f2f74d915baaa7b40ea7990e73de4fdeef45eebf
Dgordgun <georgdvorskii@gmail.com>
2024-10-27 21:21:00 +0200

    location methods

--- src/components/BOMexmple/index.js
+++ src/components/BOMexmple/index.js
@@ -1,32 +1,13 @@
 import React from 'react';
 
 class BOM extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state={
-            width:window.innerWidth,
-            height:window.innerHeight
-        }
-    }
-    componentDidMount(){
-        window.addEventListener('resize', this.changeScreen)
-    }
-    componentWillUnmount(){
-        window.removeEventListener('resize', this.changeScreen)
-    }
-    changeScreen=()=>{
-            this.setState({
-                width:window.innerWidth,
-                height:window.innerHeight
-            })
-    }
+    
     render(){
         return (
             <div>
-                <h1>{this.state.width} {this.state.height}</h1>
-                <button onClick={()=>{
-                    window.close()
-                }}>Close Window</button>
+                <button onClick={()=>{window.location.assign('https://google.com')}}>Assign</button>
+                <button onClick={()=>{window.location.reload()}}>Reload</button>
+                <button onClick={()=>{window.location.replace('https://google.com')}}>Replace</button>
             </div>
         );
     }
