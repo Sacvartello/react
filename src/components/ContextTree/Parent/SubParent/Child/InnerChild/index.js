eed1cb6eba020b61d1f3dd8110342fb32fa2b986
Dgordgun <georgdvorskii@gmail.com>
2024-10-27 11:14:21 +0200

    Theme InnerChild

--- src/components/ContextTree/Parent/SubParent/Child/InnerChild/index.js
+++ src/components/ContextTree/Parent/SubParent/Child/InnerChild/index.js
@@ -1,21 +1,29 @@
 import React from "react";
 import UserContextObj from "../../../../../../context/UserContext";
+import ThemeContextObj from "../../../../../../context/ThemeContext";
 import styles from './InnerChild.module.css'
 
 const InnerChild = (props) => {
   return (
-    <UserContextObj.Consumer>
-      {(contextValue) => {
-        return (
-          <div style={{ border: "3px solid black", padding: "25px" }}>
-            <h1>
-              {contextValue.user.firstName} {contextValue.user.lastName}
-            </h1>
-            <p>InnerChild</p>
-          </div>
-        );
-      }}
-    </UserContextObj.Consumer>
+    <ThemeContextObj.Consumer>
+        {([theme, tCallback])=>{
+            return(
+                <UserContextObj.Consumer>
+                    {(contextValue) => {
+                        return (
+                        <div style={{ border: "3px solid black", padding: "25px" }}>
+                            <h1>
+                                {contextValue.user.firstName} {contextValue.user.lastName}
+                            </h1>
+                            <p>InnerChild</p>
+                            <button onClick={tCallback}>Change Theme</button>
+                        </div>
+                        );
+                    }}
+                </UserContextObj.Consumer>
+            )
+        }}
+    </ThemeContextObj.Consumer>
   );
 };
 
