225d1e65e6bc7843b0d88d651bcbd42b278432f3
Dgordgun <georgdvorskii@gmail.com>
2024-10-28 16:40:26 +0200

    react-router-dom@5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 // import Aloha from './components/Aloha';
 // import Lamp from './components/Light/'
 // import Octopus from './components/Octopus';
-// import Tree from './components/Tree';
+import Tree from './components/Tree';
 // import Timer from './components/Timer';
 // import Counter from './components/Counter';
 // import UserForm1 from './components/UserForm1';
@@ -11,18 +11,26 @@ import React from "react";
 // import GridPage from './pages/GridPage';
 // import FlexPage from './pages/FlexPage'
 // import LoaderPage from './pages/LoaderPage';
-// import ContextTree from "./components/ContextTree";
+//import ContextTree from "./components/ContextTree";
 // import UserContextObj from "./context/UserContext";
 // import ThemeContextObj from "./context/ThemeContext";
 // import CONSTANS from "./constants";
 // const {THEMES} = CONSTANS
 import BOM from "./components/BOMexmple";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom/cjs/react-router-dom.min";
 class App extends React.Component {
   render() {
 
     return (
       <div>
-        <BOM/>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/home'><BOM/></Route>
+            <Route path='/tree'><Tree/></Route>
+          </Switch>
+          <Link to='/home'>to home</Link>
+          <Link to='/tree'>to tree</Link>
+        </BrowserRouter>
       </div>
     );
   }
